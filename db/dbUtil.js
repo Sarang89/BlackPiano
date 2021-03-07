7b24cfb114c2437d9b7b42b82e8db3c0d62261a2
Sarang Shisode <sarang89@gmail.com>
2021-03-08 04:21:37 +0530

    Root User Added

--- db/dbUtil.js
+++ db/dbUtil.js
@@ -1,10 +1,17 @@
 const MONGOOSE = require('mongoose');
 const DB_URL = process.env.DB_URL;
+const USER = require('../domain/domUser');
 
+const ADMIN = {
+    Name: "Admin",
+    Email: "admin@blackpiano.com",
+    Password: "bLACKpIANO"
+};
 
 async function dbConnect(){
     try {
         let connection = await MONGOOSE.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        USER.createUser(ADMIN);
         let DB = MONGOOSE.connection;
         DB.on('error', console.error.bind(console, 'MongoDB connection error:'));
         return connection;    
